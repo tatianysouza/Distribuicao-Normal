29cf08f8aa34f2736055dd8509c8036f09f9cbce
tatiany <tatianysouza2018@gmail.com>
2023-11-14 21:46:38 -0300

    mudando parametros

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ function gerarGrafico() {
     var media = document.getElementById("media").value;
     var desvio = document.getElementById("desvio").value;
 
+    if (isNaN(media) || isNaN(desvio)) {
+        alert("Por favor, insira números válidos para média e desvio padrão.");
+        return;
+    }
+
     function normal(x) {
         return (1 / (desvio * Math.sqrt(2 * Math.PI))) * Math.exp(-0.5 * Math.pow((x - media) / desvio, 2));
     }
@@ -11,16 +16,16 @@ function gerarGrafico() {
     var ctx = document.getElementById('grafico').getContext('2d');
 
     if (chart) {
-        chart.data.datasets[0].data = Array.from({ length: 100 }, (_, i) => normal(i - 50));
+        chart.data.datasets[0].data = Array.from({ length: 50 }, (_, i) => normal(i - 20));
         chart.update();
     } else {
         chart = new Chart(ctx, {
             type: 'line',
             data: {
-                labels: Array.from({ length: 100 }, (_, i) => i - 50),
+                labels: Array.from({ length: 50 }, (_, i) => i - 20),
                 datasets: [{
                     label: 'Distribuição Normal',
-                    data: Array.from({ length: 100 }, (_, i) => normal(i - 50)),
+                    data: Array.from({ length: 50 }, (_, i) => normal(i - 20)),
                     borderColor: 'rgb(75, 192, 192)',
                     fill: false
                 }]
